dd379f02e123c683997e81f8d57364383ca18e7b
tkn13 <thaksin.ckn@gmail.com>
2023-10-22 13:02:16 +0700

    [65050345][fetch -> main] add show user table func

--- frontend/admin/script/index.js
+++ frontend/admin/script/index.js
@@ -1,36 +1,33 @@
 var create_account_but = document.getElementById("create-account");
 var get_account_but = document.getElementById("get-account");
-var create_account_username = document.getElementById("username");
-var create_account_balance = document.getElementById("balance");
-var get_account_id = document.getElementById("id")
-var get_account_username = document.getElementById("username");
-var get_account_balance = document.getElementById("balance");
+var create_account_username = document.getElementById("create_account_username");
+var create_account_balance = document.getElementById("create_account_balance");
 
 create_account_but.addEventListener('click', async () => {
 
-    try{
-        const res = await fetch('http://localhost:3000/os-project/create-account',{
-            method : "POST",
-            body : jsonDataString
+    const data = {
+        username: create_account_username.value,
+        balance: create_account_balance.value
+    };
+    const jsonDataString = JSON.stringify(data);
+
+    try {
+        const res = await fetch('http://localhost:3000/os-project/create-account', {
+            method: "POST",
+            body: jsonDataString
         });
 
-        const data = {
-            username : create_account_username.value,
-            balance : create_account_balance.value
-        };
-        const jsonDataString = JSON.stringify(data);
+        if (res.status != 200) {
+            window.location.href = '../error.html'
+            return
+        } else {
+            window.location.href = '../suceess.html'
+        }
     } catch (error) {
         console.error('An error occurred:', error);
     }
 });
 
 get_account_but.addEventListener('click', async () => {
-    try{
-        const res = await fetch('http://localhost:3000/os-project/get-account',{
-            method : "GET"
-        });
-
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+    window.location.href = 'get-account'
 });
